c8c0ad301de8b772d3f68c442f8d67d98b375c50
Siarhei Fiedartsou <miksayer@gmail.com>
2013-11-12 22:49:36 +0300

    Folders reorganize

---
+++ Demos/iOS/GLES2Demo/FontGLES/FontGLES.h
@@ -0,0 +1,13 @@
+//
+//  FontGLES.h
+//  FontGLES
+//
+//  Created by Sergey Fedortsov on 12.11.13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FontGLES : NSObject
+
+@end
---
+++ Demos/iOS/GLES2Demo/FontGLES/FontGLES.m
@@ -0,0 +1,13 @@
+//
+//  FontGLES.m
+//  FontGLES
+//
+//  Created by Sergey Fedortsov on 12.11.13.
+//
+//
+
+#import "FontGLES.h"
+
+@implementation FontGLES
+
+@end
